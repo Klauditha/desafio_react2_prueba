d42dd424a41108c32f76420b258b1e706ae52201
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-10 18:52:23 -0300

    :lipstick: :sparkles: Imagen e ingredientes

--- src/components/PizzaCard/PizzaCard.jsx
+++ src/components/PizzaCard/PizzaCard.jsx
@@ -1,18 +1,35 @@
 /* eslint-disable react/prop-types */
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./PizzaCard.css";
+import { FaPizzaSlice } from "react-icons/fa";
 
-const PizzaCard = ({ id, name, price, image }) => {
+const PizzaCard = ({ id, name, price, image, ingredients }) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <hr></hr>
+          <h6>Ingredientes:</h6>
+          <div className="ingredientes">
+            {ingredients
+              ? ingredients.map((ingredient, index) => (
+                  <p className="itemIngredient" key={index}>
+                    <FaPizzaSlice />
+                    {ingredient}
+                  </p>
+                ))
+              : null}
+          </div>
+
+          <h1>{price}</h1>
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <div>
+          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Go somewhere</Button>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,13 @@ const Home = () => {
   return (
     <div>
       <Encabezado />
-      <PizzaCard id="1" name="Pizza Margarita" price="10.00" image="pizza-margarita.jpg"/>
+      <PizzaCard
+        id="1"
+        name="Pizza Margarita"
+        price="10.00"
+        image="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-2000595_640_c.jpg?alt=media&token=61325b6e-a1e0-441e-b3b5-7335ba13e8be"
+        ingredients={["mozzarella", "tomates", "salame"]}
+      />
     </div>
   );
 };
